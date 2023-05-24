43abbee39f7cb7ba7983ed30f4b5ed97ad46cd97
Julien Bogiraud <julien@bogiraud.com>
2023-05-24 16:36:22 +0200

    Display genres (updating grid Chakra responsive)

--- game-hub/src/components/GenreList/GenreList.tsx
+++ game-hub/src/components/GenreList/GenreList.tsx
@@ -1,15 +1,28 @@
+import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
 import userGenres from "../../hooks/useGenre";
+import getCroppedImageUrl from "../../services/image-url";
 
 const GenreList = () => {
   const { data: genres } = userGenres();
 
   return (
     <div>
-      <ul>
-        {genres.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
-        ))}
-      </ul>
+      <List>
+        {genres.map((genre) => {
+          return (
+            <ListItem key={genre.id} paddingY="5px">
+              <HStack>
+                <Image
+                  boxSize="32px"
+                  borderRadius={8}
+                  src={getCroppedImageUrl(genre.image_background)}
+                ></Image>
+                <Text fontSize="xl">{genre.name}</Text>
+              </HStack>
+            </ListItem>
+          );
+        })}
+      </List>
     </div>
   );
 };
